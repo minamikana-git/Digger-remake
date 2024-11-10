49a61b62ff712b563afecb09fc9111975ff6c377
ほたまちスバル <yuiwiz928_sub@yahoo.co.jp>
2024-11-10 15:56:54 +0900

    pre-set

---
+++ src/main/java/net/hotamachi/digger/Digger.java
@@ -0,0 +1,15 @@
+package net.hotamachi.digger;
+
+import org.bukkit.plugin.java.JavaPlugin;
+public class Digger extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+    }
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
+
